26b71bbe9ef06308434e7d4b833b2b526cdbe69f
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-06-01 22:28:15 -0300

    push

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import style from "./Layout.module.scss"
 const Layout = ({children, height, width}) => {
     return (
         <div className={style.Layout} style={{height:`${height}px`}}>
-            <div className={style.above}>HEIGHT:{height}</div>
             <div className={style.childrenContainer}>{children}</div>
             <div className={style.below}>HEIGHT:{height}</div>
         </div>
